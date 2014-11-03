ba3039d0f7f28583daa208f943efadec1ce85c96
Sandstrom <mail@a16m.se>
2014-11-03 21:33:31 +0100

    Debug preparation

--- Brocfile.js
+++ Brocfile.js
@@ -4,17 +4,34 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp();
 
-// Use `app.import` to add additional libraries to the generated
-// output files.
+// USEFUL FOR DEBUGGING
+// 
+// EmberApp.prototype.styles = function() {
+//   var addonTrees = this.addonTreesFor('styles');
+//   var external = this._processedExternalTree();
+//   var styles = pickFiles(this.trees.styles, {
+//     srcDir: '/',
+//     destDir: '/app/styles'
+//   });
 //
-// If you need to use different assets in different
-// environments, specify an object as the first parameter. That
-// object's keys should be the environment name and the values
-// should be the asset to use in that environment.
+//   var trees = [external].concat(addonTrees);
+//   trees.push(styles);
 //
-// If the library that you are including contains AMD or ES6
-// modules that you would like to import into your application
-// please specify an object with the list of modules as keys
-// along with the exports of each module as its value.
+//   var stylesAndVendor = mergeTrees(trees, {
+//     description: 'TreeMerger (stylesAndVendor)'
+//   });
+//
+//   console.info('---[ addonTrees ]---');
+//   console.info(addonTrees);
+//   console.info('---[ styles ]---');
+//   console.info(styles);
+//   console.info('---[ trees ]---');
+//   console.info(trees);
+//   console.info('---[ stylesAndVendor ]---');
+//   console.info(stylesAndVendor);
+//
+//   throw new Error('halt');
+//
+// };
 
 module.exports = app.toTree();
