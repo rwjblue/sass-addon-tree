d4194c25998b2b14382d77e933e0d4289efce7cf
Robert Jackson <robert.w.jackson@me.com>
2014-11-06 10:49:45 -0500

    Make it work.

--- Brocfile.js
+++ Brocfile.js
@@ -1,6 +1,19 @@
 /* global require, module */
 
+var path = require('path');
+
+var sassCompiler = require('broccoli-sass');
+
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+var StylePlugin = require('ember-cli/lib/preprocessors/style-plugin');
+
+StylePlugin.prototype.toTree = function(tree, inputPath, outputPath, options) {
+  var input = path.join(inputPath, 'app.scss');
+  var output = this.options.app.options.outputPaths.app.css;
+
+  return sassCompiler([tree], input, output, this.options);
+};
+
 
 var app = new EmberApp();
 
